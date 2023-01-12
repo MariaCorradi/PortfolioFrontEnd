9945da933b3cd6ba7150401a6f6b27532e15d3f5
MariaCorradi <110426444+MariaCorradi@users.noreply.github.com>
2023-01-11 23:05:21 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoAPComponent } from './component/logo-ap/logo-ap.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AcercadeComponent } from './component/acercade/acercade.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ProyectosComponent } from './component/proyectos/proyectos.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
